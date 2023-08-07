0478384a97f8dbf104e5d7d2efff3992af38e85f
cheawook <qsjh2229@naver.com>
2023-08-07 14:36:10 +0900

    서버 주소 변경

--- src/config/constans.jsx
+++ src/config/constans.jsx
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?  "https://port-0-goodsshop-sever-ac2nll0aqifh.sel4.cloudtype.app/" :"http://localhost:8080"
+export const API_URL = process.env.NODE_ENV === 'production' ?  "https://port-0-goodsshop-sever-ac2nll0aqifh.sel4.cloudtype.app" :"http://localhost:8080"
 
 
 
