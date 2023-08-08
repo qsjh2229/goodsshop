518ba955b8bcf27957a447add6aeca162c3a62bb
cheawook <qsjh2229@naver.com>
2023-08-08 18:13:14 +0900

    수정

---
+++ src/component/ItemData.jsx
@@ -0,0 +1,25 @@
+const feedsList = [
+    {
+     title:'PH 5 Hues of Orange', 
+     maindes:'덴마크 디자인 조명의 대표적인 아이콘이라고 할 수 있는 Louis Poulsen의 PH5입니다.', 
+     des:'덴마크 디자인 조명의 대표적인 아이콘이라고 할 수 있는 Louis Poulsen의 PH5입니다. PH 5는 1925-1926 three shade을 디자인한 폴 헤닝 센이 1958년에 디자인해 최다 판매를 기록한 베스트셀러 조명입니다. 전 세계적으로 가장 인기 있고 대중적이며 콜렉터와 수많은 디자인 애호가들을 매혹시킨 조명입니다. 최다 판매된 조명인 만큼 다양한 공간 어디에나 자연스럽게 어울리는 매력적인 조명입니다. 다양한 컬러로 만나보실 수 있으며 서로 다른 컬러를 믹스&매치하면 공간을 보다 더 감각적인 분위기로 연출 할 수 있는 조명입니다.', 
+     brand:'LOUIS POULSEN', 
+     img:'./img/light/s-light1.jpg', 
+     price:'1,135,000',
+     category:'light',
+     subimg1:'',
+     subimg2:'',
+     subimg3:'',
+     subimg4:'',
+     
+    },
+    {
+     title:' AJ 플로어', 
+     maindes:'', 
+     des:'', 
+     brand:'LOUIS POULSEN', 
+     img:'./img/light/s-light1.jpg', 
+     price:'935,000'
+    },
+
+  ];
