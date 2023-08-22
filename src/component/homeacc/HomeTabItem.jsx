72ca377bb07c09641b2c79d29b8167fc6105cb37
cheawook <qsjh2229@naver.com>
2023-08-22 17:31:39 +0900

    homeacc 이미지

--- src/component/homeacc/HomeTabItem.jsx
+++ src/component/homeacc/HomeTabItem.jsx
@@ -1,31 +1,35 @@
 // LightTabItem.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react"
+import { Link } from "react-router-dom"
 
 const HomeTabItem = ({ data }) => {
-  const homeItems = data.filter(item => item.category.includes('home'));
-  const addCommasToNumber = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  };
+   const homeItems = data.filter((item) => item.category.includes("home"))
+   const addCommasToNumber = (number) => {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+   }
 
-  return (
-    <div className='p-tab-item'>
-      <div className='p-tab-itembox'>
-        {homeItems.map((item, idx) => (
-          <Link to={`/detail/${item.id}`} >
-            <div className='page-tab-item'>
-              <div className='p-tab-imgbox'><img src={item.img} alt={item.title} /></div>
-              <div className='p-tab-txtbox'>
-                <span className='p-t-title'>{item.title}</span>
-                <span className='p-t-price'>{addCommasToNumber(item.price)}</span>
-                <span className='p-t-maindes'>{item.maindes}</span>
-              </div>
-            </div>
-          </Link>
-        ))}
+   return (
+      <div className='p-tab-item'>
+         <div className='p-tab-itembox'>
+            {homeItems.map((item, idx) => (
+               <Link to={`/detail/${item.id}`}>
+                  <div className='page-tab-item'>
+                     <div className='p-tab-imgbox'>
+                        <img src={item.img} alt={item.title} />
+                     </div>
+                     <div className='p-tab-txtbox'>
+                        <span className='p-t-title'>{item.title}</span>
+                        <span className='p-t-price'>
+                           {addCommasToNumber(item.price)}
+                        </span>
+                        <span className='p-t-maindes'>{item.maindes}</span>
+                     </div>
+                  </div>
+               </Link>
+            ))}
+         </div>
       </div>
-    </div>
-  );
-};
+   )
+}
 
-export default HomeTabItem;
+export default HomeTabItem
--- src/component/kitchen/KitchenTabItem.jsx
+++ src/component/kitchen/KitchenTabItem.jsx
@@ -1,30 +1,36 @@
 // LightTabItem.js
 
-import { Link } from 'react-router-dom';
-import React from 'react';
+import { Link } from "react-router-dom"
+import React from "react"
 
 const KitchenTabItem = ({ data }) => {
-  const addCommasToNumber = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  };
-  return (
-    <div className='p-tab-item'>
-      <div className='p-tab-itembox'>
-        {data.filter(item => item.category.includes('kitchen')).map((item, idx) => (
-       <Link to={`/detail/${item.id}`}>
-            <div key={item.idx} className='page-tab-item'>
-              <div className='p-tab-imgbox'><img src={item.img} alt={item.title} /></div>
-              <div className='p-tab-txtbox'>
-                <span className='p-t-title'>{item.title}</span>
-                <span className='p-t-price'>{addCommasToNumber(item.price)}</span>
-                <span className='p-t-maindes'>{item.maindes}</span>
-              </div>
-            </div>
-       </Link>
-        ))}
+   const addCommasToNumber = (number) => {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+   }
+   return (
+      <div className='p-tab-item'>
+         <div className='p-tab-itembox'>
+            {data
+               .filter((item) => item.category.includes("kitchen"))
+               .map((item, idx) => (
+                  <Link to={`/detail/${item.id}`}>
+                     <div key={item.idx} className='page-tab-item'>
+                        <div className='p-tab-imgbox'>
+                           <img src={item.img} alt={item.title} />
+                        </div>
+                        <div className='p-tab-txtbox'>
+                           <span className='p-t-title'>{item.title}</span>
+                           <span className='p-t-price'>
+                              {addCommasToNumber(item.price)}
+                           </span>
+                           <span className='p-t-maindes'>{item.maindes}</span>
+                        </div>
+                     </div>
+                  </Link>
+               ))}
+         </div>
       </div>
-    </div>
-  );
-};
+   )
+}
 
-export default KitchenTabItem;
+export default KitchenTabItem
